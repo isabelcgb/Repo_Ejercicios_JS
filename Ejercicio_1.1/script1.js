ce65f9997ecb4977e461e00cde51067ab217af57
isabelcgb <77284049+isabelcgb@users.noreply.github.com>
2021-01-25 17:03:40 +0100

    Ejercicio 1

---
+++ Ejercicio_1.1/script1.js
@@ -0,0 +1,23 @@
+// Volumen de un cubo rectangulo
+console.log('Calculo de Volumen');
+const volumen= 16*23*13;
+console.log ('Volumen =',volumen);
+console.log('Volumen = ', volumen + 'cm3');
+
+//Caluclo del Área A=al +2ab
+console.log('Calculo del Área');
+const areaLateral= 23*13;
+const areaBase= 16*23; 
+const areaTotal = areaLateral + 2*areaBase;
+console.log('Area =', areaTotal);
+
+//Calculo con variables
+const Altura = 13;
+const Base = 23;
+const Ancho = 16;
+
+const Volumen2 = Base * Altura * Ancho;
+console.log('Volumen2 = ', Volumen2)
+
+const Area2 = (Base * Altura) + 2*(Ancho * Base)
+console.log('Area2= ', Area2)
