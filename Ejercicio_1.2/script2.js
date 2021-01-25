bde14718a36f947d126877ff6d0d38ba622aa99b
isabelcgb <77284049+isabelcgb@users.noreply.github.com>
2021-01-25 17:23:58 +0100

    Ejercicio 1.2

---
+++ Ejercicio_1.2/script2.js
@@ -0,0 +1,12 @@
+console.log('¿Cuándo es tu cumpleaños?');
+// fecha de cumpleaños
+const fecha = {
+    dia:28,
+    mes:'Octubre',
+    ano: 1991,
+}
+console.log('Mi cumpleaños es el ' + fecha.dia + ' de ' + fecha.mes + ' del ' + fecha.ano);
+
+// Mes Anterior
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+console.log(meses[10]);
