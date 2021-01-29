59f9baca4d24a4e1c0f2c6ace77d8f586666d0af
isabelcgb <77284049+isabelcgb@users.noreply.github.com>
2021-01-29 19:24:11 +0100

    Ejercicio 1.15

---
+++ Ejercicio_1.15/script15.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//Variables
+const scores = [
+    {name: 'Christania Williams', country: 'Jamaica', time: 11.80},
+    {name: 'Elaine Thompson', country: 'Jamaica', time: 10.71},
+    {name: 'Tori Bowie', country: 'EEUU', time: 10.83},
+    {name: 'Marie Josée Ta Lou', country: 'Irlanda', time: 10.86},
+    {name: 'Dafne Schippers', country: 'Holanda', time: 10.90},
+    {name: 'English Gardner', country: 'EEUU', time: 10.94},
+    {name: 'Michelle-Lee Ahye', country: 'Trinidad', time: 10.92},
+    {name: 'Shelly-Ann Fraser-Pryce', country: 'Jamaica', time: 10.86}
+]
+
+//Funcion Ganadora
+
+const Ganadora = scores.reduce((acumulator, previousValue) => {
+    if (acumulator.time < previousValue.time) {
+        return acumulator;
+    }
+    else {
+        return previousValue;
+    }
+}, {});
+console.log ('El ganador es : ', Ganadora)
